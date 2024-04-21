b5b09c88a7c2f8f95834d46cbc8e8a94fd7d6425
dmytrozuzuk <dmytrozuzuk@planeks.net>
2024-04-21 20:27:23 +0300

    fix

--- scripts/kubeplugin.sh
+++ scripts/kubeplugin.sh
@@ -1,11 +1,15 @@
-kubectl top $1 $0 -n $2 | tail -n +2 | while read line
-do
+#!/bin/bash
+
+# Set the namespace to query (you might need to adjust this based on your requirements)
+NAMESPACE="k3d-demo"
+
+# Use kubectl top to get resource usage statistics for all pods in the specified namespace
+kubectl top pod -n "$NAMESPACE" | tail -n +2 | while read line; do
   # Extract CPU and memory usage from the output
-  NAME=$(echo $line | awk '{print $1}')
-  CPU=$(echo $line | awk '{print $2}')
-  MEMORY=$(echo $line | awk '{print $3}')
+  NAME=$(echo "$line" | awk '{print $1}')
+  CPU=$(echo "$line" | awk '{print $2}')
+  MEMORY=$(echo "$line" | awk '{print $3}')
 
   # Output the statistics to the console
-  echo "$0, $2, $NAME, $CPU, $MEMORY"
+  echo "$0, $NAMESPACE, $NAME, $CPU, $MEMORY"
 done
-

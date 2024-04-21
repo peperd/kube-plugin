41262c8f69dee1dda42fbc45f291abd3a0dd2732
dmytrozuzuk <dmytrozuzuk@planeks.net>
2024-04-21 19:27:37 +0300

    init

---
+++ scripts/kubeplugin.sh
@@ -0,0 +1,11 @@
+kubectl top $1 $0 -n $2 | tail -n +2 | while read line
+do
+  # Extract CPU and memory usage from the output
+  NAME=$(echo $line | awk '{print $1}')
+  CPU=$(echo $line | awk '{print $2}')
+  MEMORY=$(echo $line | awk '{print $3}')
+
+  # Output the statistics to the console
+  echo "$0, $2, $NAME, $CPU, $MEMORY"
+done
+
